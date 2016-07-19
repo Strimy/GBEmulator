9f141c472b7beeb1a6728114902b10054c03a3cd
Adrien Olivencia <adrien@kikoocorp.org>
2016-07-19 20:58:35 +0200

    Init

---
+++ Emulator/Emulator.GB.Core/CPU.cs
@@ -0,0 +1,183 @@
+﻿using Emulator.GB.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emulator.GB.Core
+{
+    public class CPU : ICpu
+    {
+        public byte A
+        {
+            get;  private set;
+        }
+
+        public short AF
+        {
+            get
+            {
+                return (short)(A << 8 | F);
+            }
+        }
+
+        public byte B
+        {
+            get; private set;
+        }
+
+        public short BC
+        {
+            get
+            {
+                return (short)(B << 8 | C);
+            }
+        }
+
+        public byte C
+        {
+            get; set;
+        }
+
+        public bool CarryFlag
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public byte D
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public short DE
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public byte E
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public byte F
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public byte H
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool HalfCarryFlag
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public short HL
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public byte L
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int LastOpTime
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IMMU MMU
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public short PC
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public short SP
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool SubstractFlag
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public bool ZeroFlag
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Exec(int opCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetMMU(IMMU mmu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetRegister(ref byte b, byte value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Emulator/Emulator.GB.Interfaces/ICpu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Emulator.GB.Interfaces
+{
+    public interface ICpu
+    {
+        #region Registers
+        short AF { get; }
+        short BC { get; }
+        short DE { get; }
+        short HL { get; }
+
+        byte A { get; }
+        byte B { get; }
+        byte C { get; }
+        byte D { get; }
+        byte E { get; }
+        byte F { get; }
+        byte H { get; }
+        byte L { get; }
+
+
+        /// <summary>
+        /// Program Counter - Instruction Pointer / Points to the next instruction to be executed in the GB memory
+        /// </summary>
+        short PC { get; }
+
+        /// <summary>
+        /// Stack pointer
+        /// </summary>  
+        short SP { get; }
+
+        bool ZeroFlag { get; }
+
+        bool SubstractFlag { get; }
+
+        bool HalfCarryFlag { get; }
+
+        bool CarryFlag { get; }
+        #endregion
+
+        IMMU MMU { get; }
+
+        int LastOpTime { get; }
+        void Exec(int opCode);
+
+        void Init();
+
+        #region Debugs interface
+        void SetMMU(IMMU mmu);
+        void SetRegister(ref byte b, byte value);
+        #endregion
+    }
+}
